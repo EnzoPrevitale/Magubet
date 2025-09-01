8fa7c5245f88edc3c1e3080695d746cda0acf61a
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-01 14:29:13 -0300

    Magubet

---
+++ promises.js
@@ -0,0 +1,40 @@
+let saldo = 100;
+let odd;
+
+const btn = document.getElementById("aposta");
+const dinheiro = document.getElementById("saldo");
+const body = document.querySelector("body");
+
+dinheiro.innerHTML = "Saldo: R$" + saldo;
+
+let fazerAposta = (valor) => new Promise((resolve, reject) => {
+    console.log("Esperando o fim do jogo...");
+    sorteio = Math.random();
+    if(saldo >= valor) {
+        saldo -= valor;
+        if(sorteio < 0.3) {
+            let valorGanho = valor * odd;
+            saldo += valorGanho;
+            resolve("Parabéns! Você ganhou R$" + valorGanho);
+        } else {
+            resolve("OTÁRIO!!! Você perdeu R$" + valor + ".");
+        }
+        dinheiro.innerHTML = "Saldo: R$" + saldo;
+    } else {
+            reject("Você está duro e não pode apostar essa quantia. Aposte valores mais baixos para conseguir dinheiro.");
+    }
+
+    if(saldo <= 0) {
+            body.style.backgroundColor = "red";
+            body.innerHTML = "<h1>VOCÊ PERDEU!!!</h1>";
+            body.querySelector("h1").style.textAlign = "center";
+            reject("Você perdeu!!!");
+        }
+});
+
+btn.addEventListener("click", () => {
+    odd = Math.floor(Math.random() * 10) + 1;
+    fazerAposta(Number(prompt("Digite o valor que deseja apostar. Odd: " + odd)))
+        .then(alert)
+        .catch(alert)
+    });
