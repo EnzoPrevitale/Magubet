bdc6f6c1b3f28b18c6d2d79b8bc173efc2806016
EnzoPrevitale <enzogabrielprevitale@gmail.com>
2025-09-01 16:00:00 -0300

    odds atualizadas

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ let fazerAposta = (valor) => new Promise((resolve, reject) => {
 });
 
 btn.addEventListener("click", () => {
-    odd = Math.floor(Math.random() * 10) + 1;
+    odd = Math.floor(Math.random() * 9) + 2;
     fazerAposta(Number(prompt("Digite o valor que deseja apostar. Odd: " + odd)))
         .then(alert)
         .catch(alert)
